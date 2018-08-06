e2db5c192dab7e024a2b5be2314ccbc897f13bf8
Heinson King <HeinsonJ.King@pccw.com>
2018-08-06 11:01:00 +0800

    change process.env.port to process.env.PORT

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
-const port = process.env.port;
+const port = process.env.PORT;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
