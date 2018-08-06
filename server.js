c637988b7b0851f18d7df91ab4a5d9818acabc9f
Heinson King <HeinsonJ.King@pccw.com>
2018-08-06 11:13:20 +0800

    update process.env.PORT

--- server.js
+++ server.js
@@ -55,6 +55,6 @@ app.get('/bad', (req, res) => {
     });
 });
 
-app.listen(3000 || port, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log(`Server is up on port ${port}`);
 });
