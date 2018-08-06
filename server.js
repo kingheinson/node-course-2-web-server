a2edb7685d6ad17a64728a68a0a78e8286dc4200
Heinson King <HeinsonJ.King@pccw.com>
2018-08-06 11:20:28 +0800

    update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
@@ -55,6 +55,6 @@ app.get('/bad', (req, res) => {
     });
 });
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(port,"0.0.0.0", () => {
     console.log(`Server is up on port ${port}`);
 });
