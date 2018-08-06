5abe5133f677e95b4f4a7716eb5ca7edba3eed00
Heinson King <HeinsonJ.King@pccw.com>
2018-08-06 14:26:39 +0800

    Add project page

--- server.js
+++ server.js
@@ -48,6 +48,12 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects'
+    });
+});
+
 // /bad -send back json with ErrorMessage
 app.get('/bad', (req, res) => {
     res.send({
